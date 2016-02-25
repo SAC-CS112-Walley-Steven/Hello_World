7c44781d15243cb654d4a073aba5d64c433cb065
SAC-CS112-Walley-Steven <swalley2626@yahoo.com>
2016-02-24 21:28:42 -0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,13 +1,26 @@
 // Hello.java
 // 1st in class lab
 
+import java.util.Scanner;
+
 public class Hello
 {
 	public static void main (String [] args)
 	{
-		String x = "Hello", y = "World";
+		Scanner input = new Scanner (System.in);
 		
-		System.out.println ("x + y");
+		int x, y, sum;
+		
+		System.out.print ("Please enter a number: ");
+		x = input.nextint();
+		
+		System.out.print ("Please enter one last number and I'll add them together:  ");
+		y = input.nextint();
+		
+		sum = x + y;
+		
+		System.out.printf ("The sum of your two number is %d\n", sum);
+		
 		
 	}
 }
