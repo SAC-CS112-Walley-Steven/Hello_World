885fa3c36c18c3ed33ef58a89f1331dcaf62da96
SAC-CS112-Walley-Steven <swalley2626@yahoo.com>
2016-03-19 16:31:22 -0700

    Added files via upload

--- GuessGameGUI.java
+++ GuessGameGUI.java
@@ -12,26 +12,28 @@ public class GuessGameGUI
 		int userInput=0, count=0;
 		
 		String name = JOptionPane.showInputDialog("Please enter your name");
-		String intro = String.format("Hi %s!\nWelcome to the Guess-A-Number game.", name);
+		String intro = String.format("Hi %s, welcome to the Guess-A-Number game!", name);
 
 		JOptionPane.showMessageDialog(null, intro);
 		
 		
 		while (userInput != computerNum)
-				{
-				userInput = Integer.parseInt( JOptionPane.showInputDialog("Please enter a number between 1 and 10"));
-					count++;
-					String low = String.format("%d pick:   %d  is too small. Please try again.", count, userInput);		
-					String winMessage = String.format
-							("Congrats. You guessed it!\nThe correct number is %d.\n\nThank you for playing.", computerNum);
-					String high = String.format("%d pick:   %d  is too large. Please try again.", count, userInput);
+			{
+			userInput = Integer.parseInt( JOptionPane.showInputDialog("Please enter a number between 1 and 10"));
 		
-					if (userInput == computerNum)
+			count++;
+			
+			String low = String.format("%d pick:   %d  is too small. Please try again.", count, userInput);		
+			String winMessage = String.format
+					("Congrats. You guessed it!\nThe correct number is %d.\n\nThank you for playing.", computerNum);
+			String high = String.format("%d pick:   %d  is too large. Please try again.", count, userInput);
+		
+				if (userInput == computerNum)
 						JOptionPane.showMessageDialog(null, winMessage);
-					else if (userInput > computerNum)
+				else if (userInput > computerNum)
 						JOptionPane.showMessageDialog(null, high);
-					else
+				else
 						JOptionPane.showMessageDialog(null, low);
-				}			
+			}			
 	}
 }
