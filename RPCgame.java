6a67be504c395741a0c5fc6e99094ac8e37e77ea
SAC-CS112-Walley-Steven <swalley2626@yahoo.com>
2016-03-21 22:52:01 -0700

    Added files via upload

--- RPCgame.java
+++ RPCgame.java
@@ -11,6 +11,7 @@ public class RPCgame {
 		String name;
 		int userInput=0, decision=0;
 		int gameCounter = 1;
+		double winPercent, win=0;
 		
 		System.out.print("Please enter name: ");
 		name=input.nextLine();
@@ -26,33 +27,57 @@ public class RPCgame {
 		while (decision ==1)
 		{
 			
-			
+			computerPick = 1+(int)(Math.random()*3);
 			System.out.printf("\nGame %d\nPress 1 for Rock, 2 for Paper, 3 for Scissor: \n", gameCounter);
 			userInput=input.nextInt();
 			
 			gameCounter ++;
 			
+			
 			if (userInput == computerPick)
+			{
 				System.out.printf("It's a Tie.\nWe both chose # %d", computerPick);
+				--gameCounter;
+			}
 			if ((userInput==1)&& (computerPick == 2))
 				System.out.printf("Im sorry, you lose. You chose # %d, I chose # %d\nPaper beats Rock", userInput, computerPick);
 			if ((userInput==1)&&(computerPick==3))
+			{
 				System.out.printf("Congrats, you win! You chose # %d, I chose # %d\nRock beats Scissor", userInput, computerPick);
+				++win;
+			}
 			if ((userInput==2)&& (computerPick==1))
+			{
 				System.out.printf("Congrats, you win! You chose # %d, I chose # %d\nPaper beats Rock", userInput, computerPick);
+				++win;
+			}
 			if ((userInput==2)&&(computerPick==3))	
 				System.out.printf("Im sorry, you lose. You chose # %d, I chose # %d\nScissor beats Paper", userInput, computerPick);
 			if ((userInput==3)&&(computerPick==1))	
 				System.out.printf("Im sorry, you lose. You chose # %d, I chose # %d\nRock beats Scissor", userInput, computerPick);
 			if ((userInput==3)&&(computerPick==2))	
+			{
 				System.out.printf("Congrats, you win! You chose # %d, I chose # %d\nScissor beats Paper", userInput, computerPick);
-		
-			System.out.print("\n\nPress 1 to play again, 2 to quit: ");
+				++win;
+			}
+			
+			System.out.print("\n\nPress 1 to play again, 2 to calculate results, 3 to quit: ");
 			decision = input.nextInt();
 			
-			if (decision ==2)
-				System.out.println("\nThank you! Have a nice day.");
+			if (decision == 2)
+			{
+				winPercent = (win/(double)(gameCounter-1))*100;
+				System.out.println("\n\nCalculating results..");
+				System.out.printf("\nYou won %.0f out of %d games for a winning percentage of %.0f%%\n", win, (gameCounter-1), winPercent);
+				if (winPercent > 0.50)
+					System.out.println("\nNice job! Keep up the good work!");
+				else
+					System.out.println("\nYou need more practise..");
+			}
 			
+			if (decision ==3)
+				System.out.println("\nThank you! Have a nice day.");
+		
 		}
 					
 	}
