d22863480c9f25a5d89b549a21cd0deb828c9e26
Steven Walley <swalley2626@yahoo.com>
2016-03-25 20:28:28 -0700

    Added files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,3 @@
-//Calculator.java
-
 import java.util.Scanner;
 
 public class Calculator {
@@ -8,55 +6,37 @@ public class Calculator {
 	{
 		Scanner input = new Scanner (System.in);
 		
-		int decision, operation; 
-		String name;
-		double quotient, number1, number2, sum, product, difference;
+		int operationValue=0;
 		
-
-		System.out.print("Please enter you name: ");
-		name = input.nextLine();
+		System.out.println("Calculator Program");
 		
-		System.out.printf("Hi %s, this program can calculate two numbers at a time only.", name);
-		
-		System.out.print("\n\nPress 1 to continue or 0 to end program: ");
-		decision = input.nextInt();
-		
-		if (decision == 0)
-			System.out.println("\nThank you. Have a nice day!");
-		
-		while (decision != 0)
-		{		
-			System.out.print("\nPlease enter the first number: ");
-			number1 = input.nextDouble();
-		
-			System.out.print("Please enter the second number: ");
-			number2 = input.nextDouble();
-		
-			sum = number1+number2;
-			difference = number1-number2;
-			product = number1*number2;
-			quotient = number1 / number2;
-		
-			System.out.print("Press 1 for addition, 2 for subtraction, 3 for multiplication, or 4 for division: ");
-			operation = input.nextInt();
-		
-			if (operation == 1)
-			System.out.printf("\nAdding the two numbers yields %f\n", sum);
-			if (operation == 2)
-			System.out.printf("\nSubtracting the two numbers yields %f\n", difference);
-			if (operation == 3)
-			System.out.printf("\nMuliplying the two numbers yields %f\n", product);
-			if (operation == 4)
-			System.out.printf("\nDividing the two numbers yields %f\n", quotient);
+			System.out.print("\nPress + to add, - to subtract, * to multiply, / to divide: ");
+			String operation = input.nextLine();
 			
-			System.out.print("\nPress 1 to continue or 0 to end program: ");
-			decision = input.nextInt();
+			System.out.print("\nEnter first number: ");
+			double number1 = input.nextDouble();
 			
-			if (decision == 0)
-				System.out.println("\nThank you. Have a nice day!");
-					
-		}	
-					
-	}
-
+			System.out.print("Enter second number: ");
+			double number2 = input.nextDouble();
+			
+			if (operation.equals("+")) operationValue=1;
+			if (operation.equals("-")) operationValue=2;
+			if (operation.equals("*")) operationValue=3;
+			if (operation.equals("/")) operationValue=4;
+			
+			double sum = number1+number2;
+			double difference = number1-number2;
+			double product = number1*number2;
+			double quotient = number1 / number2;
+		
+			if (operationValue==1)
+				System.out.printf("\n%.2f + %.2f = %.2f", number1, number2, sum);
+			if (operationValue==2)
+				System.out.printf("\n%.2f - %.2f = %.2f", number1, number2, difference);
+			else if (operationValue==3)
+				System.out.printf("\n%.2f * %.2f = %.2f", number1, number2, product);
+			else
+				System.out.printf("\n%.2f / %.2f = %.2f", number1, number2, quotient);
+			
+}
 }
