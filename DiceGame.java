cad4cac9fdf3eeb9636700bf9615f1637f914c2c
Steven Walley <swalley2626@yahoo.com>
2016-03-31 09:56:29 -0700

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -10,7 +10,7 @@ public class DiceGame {
 		int playAgain, roll, userInput, gameCount=1;
 		String userChoice = null;
 		double win=0, evenWin = 0, oddWin = 0;
-	
+		double evenGuess = 0, oddDice=0;
 		
 		System.out.println("This is a dice game. Guess if the dice will be odd or even.");
 		
@@ -38,21 +38,23 @@ public class DiceGame {
 				if (diceNum == 1 || diceNum ==3 || diceNum ==5)
 				{
 					System.out.println("Congratulations! You guessed it. The dice rolled an odd number.");
-					++win; ++oddWin;
+					++win; ++oddWin; ++oddDice;
 				}
 				else
 					System.out.println("Sorry, you lose. The dice rolled an even number.");
 			}
 			else if (userInput ==2)
-			{
+			{	++evenGuess;
 				if (diceNum == 2 || diceNum ==4 || diceNum == 6)
 				{
 					System.out.println("Congratulations! You guessed it. The dice rolled an even number.");
 					++win; ++evenWin;
 				}
 				else
+				{	
 					System.out.println("Sorry, you lose. The dice rolled an odd number.");
-					
+					++oddDice;
+				}
 			}
 			
 			System.out.print("\n\nPress 1 to play again or 2 to exit and calculate results: ");
@@ -62,8 +64,11 @@ public class DiceGame {
 			if (playAgain == 2)
 			{
 				double winPercent = (win/(double)(gameCount-1))*100;
+				double evenGuessPercent = (evenGuess/(double)(gameCount-1))*100;
+				double oddDicePercent = (oddDice/(double)(gameCount-1))*100;
 				System.out.println("\n\nCalculating results..\n");
 				System.out.printf("You won %.0f out of %d games for a winning percentage of %.0f%%.", win, (gameCount-1), winPercent);
+				System.out.printf("\nYou chose even %.0f%% of the time.\nThe dice rolled an odd number %.0f%% of the time.", evenGuessPercent, oddDicePercent);
 				System.out.printf("\nWhen choosing even, you won %.0f times.\nWhen choosing odd, you won %.0f times.", evenWin, oddWin);
 				System.out.println("\n\nThank you! Have a nice day");
 			}
